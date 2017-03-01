9417817c0279f27ac28dc9fcff9d4bf1a1dba7c9
James Webb <webbfamily@Jamess-MacBook-Pro.local>
2017-03-01 07:18:53 -0500

    last test scrap before class

--- scrap.rb
+++ scrap.rb
@@ -5,3 +5,13 @@ require "sqlite3"
 
 # Open a database
 db = SQLite3::Database.new "time_entries.sqlite3"
+
+#Find all developers in the "Ohio sheep" group.
+sql = "SELECT developers.name
+FROM developers
+WHERE groups.name='Ohio sheep'
+JOIN groups
+ON group_assignments.group_id=groups.id
+OUTER JOIN group_assignments
+ON group_assignments.developer_id=developers.id"
+p db.execute(sql)
